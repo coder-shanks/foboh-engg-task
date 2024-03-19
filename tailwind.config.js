5f1d9e52f04a633c4335bf9e440876cf3925bb78
Shubham Tarade <sgstarade@gmail.com>
2024-03-19 23:29:58 +0530

    add custom colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      fontFamily: { inter: ['Inter', 'sans-serif'] }
+      fontFamily: { inter: ['Inter', 'sans-serif'] },
+      colors: {
+        primary: '#147D73',
+        'gray-lightest': '#F8FAFC',
+        'gray-darkest': '#212B36',
+        'blue-light': '#B2C4D4',
+        'blue-gray': '#637381',
+        'gray-bg': '#F0F0F0',
+        'gray-border': '#E7E7E7',
+        'blue-lighter': '#d4e4f2',
+        'green-lightest': '#f4fff7',
+        'green-dark': '#08822a',
+        'red-lightest': '#DC3545'
+      }
     }
   },
   plugins: []
